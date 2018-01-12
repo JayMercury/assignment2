ef5112fe2178738bb2d6191bb4bb16c5b9cd0dd2
Christopher Ng <christopherng1990@gmail.com>
2018-01-12 12:25:37 -0500

    Git Repo Testing

---
+++ r.m
@@ -0,0 +1,7 @@
+function [ y ] = r( x )
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+y = rand(x);
+
+end
+
---
+++ simple.m
@@ -0,0 +1,9 @@
+function [ y ] = simple( x )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+y = x*2;
+
+y = r(x);
+end
+
