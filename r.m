065fe9b5e39961e6cfbcf1c7a4ec8da732b18587
Christopher Ng <christopherng1990@gmail.com>
2018-01-12 12:55:41 -0500

    Git Repo delete testing

--- r.m
+++
@@ -1,7 +0,0 @@
-function [ y ] = r( x )
-%UNTITLED5 Summary of this function goes here
-%   Detailed explanation goes here
-y = rand(x);
-
-end
-
--- simple.m
+++
@@ -1,9 +0,0 @@
-function [ y ] = simple( x )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-y = x*2;
-
-y = r(x);
-end
-
