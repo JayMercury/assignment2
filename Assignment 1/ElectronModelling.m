0198162b9c383d9807ebaec092e816f983cedf63
Christopher Ng <christopherng1990@gmail.com>
2018-01-30 17:22:51 -0500

    Part 3 and fixing other parts

--- Assignment 1/ElectronModelling.m
+++ Assignment 1/ElectronModelling.m
@@ -28,14 +28,15 @@ deltaT = (L/100)/vth_e;
 figure(1)
 hold on
 subplot(3, 1, 1);
-histogram(vth_ex, 50)
+histogram(vth_ex, 100)
 title('X-component of thermal velocity');
 subplot(3, 1, 2);
-histogram(vth_ey, 50)
+histogram(vth_ey, 100)
 title('Y-component of thermal velocity');
 subplot(3, 1, 3);
-histogram(vthav, 50)
+histogram(vthav, 100)
 title('Average of thermal velocity');
+hold off
 
 % Electrons Defining
 Elec = zeros(num, 4);
@@ -49,50 +50,78 @@ t = 1e-10;                          % Total Time
 dt = 1e-15;                         % Time Step
 Psat = 1 - exp(-deltaT/tmn);        % Exponential Scattering Probability
 numplot = 5;                        % Number of electron plotted
-colorstring = 'kbgry';              % Colour Setup
+color = hsv(numplot);              % Colour Setup
 figure(2)
 for n = 0:dt:t
     
-    % Part 2 Simulation
-    if Psat > rand()
-        vth_ex = (vth_e/sqrt(2))*randn(num, 1); 
-        vth_ey = (vth_e/sqrt(2))*randn(num, 1);
-        Elec(:, 3) = vth_ex;
-        Elec(:, 4) = vth_ey;
-    end
+%     % Part 2 Simulation
+%     if Psat > rand()
+%         vth_ex = (vth_e/sqrt(2))*randn(num, 1); 
+%         vth_ey = (vth_e/sqrt(2))*randn(num, 1);
+%         Elec(:, 3) = vth_ex;
+%         Elec(:, 4) = vth_ey;
+%     end
+    
+    % Part 3 simulation, setting up boundaries for electrons spawning
+%     for m = 1:1:num
+%         if Elec(m, 1) > 80e-9 || Elec(m, 1) < 120e-9
+%             vth_ex = (vth_e/sqrt(2))*randn(num, 1);
+%             Elec(:, 3) = vth_ex;
+%         end
+%         if Elec(m, 2) > 60e-9 || Elec(m, 2) < 40e-9
+%             vth_ey = (vth_e/sqrt(2))*randn(num, 1);
+%             Elec(:, 4) = vth_ey;
+%         end
+%     end
+    
+    % Part 3 Simulation, setting up rectangles boundary
+%     subplot(2, 1, 1);
+    rectangle('Position', [80e-9 0 40e-9 40e-9])
+    rectangle('Position', [80e-9 60e-9 40e-9 40e-9])
+    hold on
     
     %Plotting limited amount of electrons
-    for p = 1:1:numplot
-        subplot(2, 1, 1);
+    for q = 1:1:numplot
+%         subplot(2, 1, 1);
         title('Electrons movement');
-        plot(Elec(p, 1), Elec(p,2), 'b.')
-        Elec(p, 1) = Elec(p, 1)+ Elec(p, 3)*dt;
-        Elec(p, 2) = Elec(p, 2)+ Elec(p, 4)*dt;
+        plot(Elec(q, 1), Elec(q,2), 'color', color(q, :))
         xlim([0 L])
         ylim([0 W])
         hold on
-    end
-    % Setting up boundaries
-    for m = 1:1:numplot
-        % Looping on x-axis
-        if Elec(m, 1) > L                       
-            Elec(m, 1) = Elec(m, 1) - L;
+        for p = 1:1:num
+            Elec(p, 1) = Elec(p, 1)+ Elec(p, 3)*dt;
+            Elec(p, 2) = Elec(p, 2)+ Elec(p, 4)*dt;
         end
-        if Elec(m, 1) < 0
-            Elec(m, 1) = Elec(m, 1) + L;
+        pause(1e-15)
+    end
+    % Setting up the boundaries
+    for o = 1:1:num
+        % Looping on x-axis
+        if Elec(o, 1) > L                       
+            Elec(o, 1) = Elec(o, 1) - L;
+        end
+        if Elec(o, 1) < 0
+            Elec(o, 1) = Elec(o, 1) + L;
         end
         % Reflecting on y-axis
-        if Elec(m, 2) > W || Elec(m, 2) < 0
-            Elec(m, 4) = -1*Elec(m, 4);
+        if Elec(o, 2) > W || Elec(o, 2) < 0
+            Elec(o, 4) = -1*Elec(o, 4);
         end
+        % Part 3 Simulation, boundaries for electrons movements
+%         if Elec(o, 1) > 80e-9 || Elec(o, 1) < 120e-9
+%             Elec(o, 3) = -1*Elec(o, 3);
+%         end
+%         if Elec(o, 2) > 60e-9 || Elec(o, 2) < 40e-9
+%             Elec(o, 4) = -1*Elec(o, 4);
+%         end
     end
     
-    % Plotting average temperature 
-    vthav = mean(sqrt(vth_ex.^2 + vth_ey.^2)); % Average thermal velocity     
-    aveT = (0.5*m_0*vthav^2)/kb;               % Average temperature
-    subplot(2, 1, 2)
-    plot(n, aveT, 'r.')
-    title('Average temperature');
-    hold on
-    pause(1e-9)
+%     % Plotting average temperature 
+%     vthav = mean(sqrt(vth_ex.^2 + vth_ey.^2)); % Average thermal velocity     
+%     aveT = (0.5*m_0*vthav^2)/kb;               % Average temperature
+%     subplot(2, 1, 2)
+%     plot(n, aveT, 'r.')
+%     title('Average temperature');
+%     hold on
+%     pause(1e-100)
 end
