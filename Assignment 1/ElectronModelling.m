b47134120a6caf456018316ddcf0cd49b752c515
Christopher Ng <christopherng1990@gmail.com>
2018-02-02 14:39:33 -0500

    All done, Parts seperated

--- Assignment 1/ElectronModelling.m
+++ Assignment 1/ElectronModelling.m
@@ -5,33 +5,32 @@ clear
 q_0 = 1.60217653e-19;                   % electron charge
 m_0 = 9.10938215e-31;                   % electron mass
 kb = 1.3806504e-23;                     % Boltzmann constant
-tmn = 0.2e-12;                           % mean time between collisions
+tmn = 0.2e-12;                          % mean time between collisions
 
 % Region Defining
 L = 200e-9;
 W = 100e-9;
 
 % Current Condition and variables
-num = 1e4;                              % Number of electrons
+num = 1e3;                              % Number of electrons
 T = 300;                                % Temperature (Kelvin)
 vth_e = sqrt((2*kb*T)/(m_0));           % Thermal velocity of an electron
 vth_ex = (vth_e/sqrt(2))*randn(num, 1); % X-component of thermal velocity
 vth_ey = (vth_e/sqrt(2))*randn(num, 1); % Y-component of thermal velocity
 vthav = sqrt(vth_ex.^2+vth_ey.^2);      % Average of thermal velocity
-deltaT = (L/100)/vth_e;
 
 % Histogram for thermal velocity
-figure(1);
-hold on
-subplot(3, 1, 1);
-histogram(vth_ex, 100)
-title('X-component of thermal velocity');
-subplot(3, 1, 2);
-histogram(vth_ey, 100)
-title('Y-component of thermal velocity');
-subplot(3, 1, 3);
-histogram(vthav, 100)
-title('Average of thermal velocity');
+% figure(1);
+% hold on
+% subplot(3, 1, 1);
+% histogram(vth_ex, 50)
+% title('X-component of thermal velocity');
+% subplot(3, 1, 2);
+% histogram(vth_ey, 50)
+% title('Y-component of thermal velocity');
+% subplot(3, 1, 3);
+% histogram(vthav, 50)
+% title('Average of thermal velocity');
 
 % Electrons Defining
 Elec = zeros(num, 4);
@@ -44,27 +43,24 @@ previous = Elec;
 
 % Electron simulation
 figure(2);
-t = 1e-3;                          % Total Time
+t = 1e-3;                           % Total Time
 dt = 1e-14;                         % Time Step
-Psat = 1 - exp(-deltaT/tmn);        % Exponential Scattering Probability
+Psat = 1 - exp(-dt/tmn);            % Exponential Scattering Probability
 numplot = 5;                        % Number of electron plotted
-color = hsv(numplot);              % Colour Setup
+color = hsv(numplot);               % Colour Setup
 
-% Part 3 simulation, setting up limitation for electrons spawning
-for m = 1:1:num
-    if Elec(m, 1) > 80e-9 && Elec(m, 1) < 120e-9
-        Elec(m, 1) = L*rand();
-        if Elec(m, 2) > 60e-9 && Elec(m, 2) < 40e-9
-            Elec(m, 2) = W*rand();
-        end
-    end
-end
+% % Part 3 simulation, setting up limitation for electrons spawning
+% for m = 1:1:num
+%     if Elec(m, 1) > 80e-9 && Elec(m, 1) < 120e-9 && (Elec(m, 2) > 60e-9 || Elec(m, 2) < 40e-9)
+%             Elec(m, 1) = L*rand();
+%             Elec(m, 2) = W*rand();
+%     end
+% end
 
-    % Part 3 Simulation, setting up rectangles boundaries
-%     subplot(2, 1, 1);
-    rectangle('Position', [80e-9 0 40e-9 40e-9])
-    rectangle('Position', [80e-9 60e-9 40e-9 40e-9])
-    hold on
+% % Part 3 Simulation, setting up rectangles boundaries
+% rectangle('Position', [80e-9 0 40e-9 40e-9])
+% rectangle('Position', [80e-9 60e-9 40e-9 40e-9])
+% hold on
     
 for n = 0:dt:t
     
@@ -76,22 +72,23 @@ for n = 0:dt:t
         Elec(:, 4) = vth_ey;
     end
            
-    %Plotting limited amount of electrons
+    for p = 1:1:num
+        previous(p, 1) = Elec(p, 1);
+        previous(p, 2) = Elec(p, 2);
+        Elec(p, 1) = Elec(p, 1)+ Elec(p, 3)*dt;
+        Elec(p, 2) = Elec(p, 2)+ Elec(p, 4)*dt;
+    end
+    
+    % Plotting limited amount of electrons
+    figure(2)
     for q = 1:1:numplot
-%         subplot(2, 1, 1);
         title('Electrons movement');
         plot([previous(q, 1), Elec(q, 1)], [previous(q, 2), Elec(q,2)], 'color', color(q, :))
         xlim([0 L])
         ylim([0 W])
         hold on
-        for p = 1:1:num
-            previous(p, 1) = Elec(p, 1);
-            previous(p, 2) = Elec(p, 2);            
-            Elec(p, 1) = Elec(p, 1)+ Elec(p, 3)*dt;
-            Elec(p, 2) = Elec(p, 2)+ Elec(p, 4)*dt;
-        end
     end
-
+    
     % Setting up the boundaries
     for o = 1:1:num
         % Looping on x-axis
@@ -107,25 +104,37 @@ for n = 0:dt:t
         if Elec(o, 2) > W || Elec(o, 2) < 0
             Elec(o, 4) = -1*Elec(o, 4);
         end
-        % Part 3 Simulation, boundaries for electrons movements
-        if Elec(o, 1) <= 120e-9 
-            if Elec(o, 1) >= 80e-9
-                Elec(o, 3) = -1*Elec(o, 3);
-            end
-        end
-%         if Elec(o, 2) > 60e-9
+%         % Part 3 Simulation, boundaries for electrons movements
+%         if Elec(o, 1) > 80e-9 && Elec(o, 1) < 120e-9 && (Elec(o, 2) < 40e-9 || Elec(o, 2) > 60e-9)
 %             if Elec(o, 2) < 40e-9
-%                 Elec(o, 4) = -1*Elec(o, 4);
+%                 if previous(o, 2) > 40e-9
+%                     Elec(o, 4) = -1*Elec(o, 4);
+%                 else
+%                     Elec(o, 3) = -1*Elec(o, 3);
+%                 end
+%             end
+%             if Elec(o, 2) > 60e-9
+%                 if previous(o, 2) < 60e-9
+%                     Elec(o, 4) = -1*Elec(o, 4);
+%                 else
+%                     Elec(o, 3) = -1*Elec(o, 3);
+%                 end
 %             end
 %         end
     end
     
-%     % Plotting average temperature 
-%     vthav = mean(sqrt(vth_ex.^2 + vth_ey.^2)); % Average thermal velocity     
+%     % Plotting average temperature
+%     vthav = mean(sqrt(vth_ex.^2 + vth_ey.^2)); % Average thermal velocity
 %     aveT = (0.5*m_0*vthav^2)/kb;               % Average temperature
-%     subplot(2, 1, 2)
+%     figure(3)
 %     plot(n, aveT, 'r.')
 %     title('Average temperature');
 %     hold on
-    pause(1e-10)
+%     pause(1e-10)
 end
+
+% % Density Plot
+% Den = hist3(Elec(:, 1:2), 'Nbins', [20, 10]);
+% figure(4);
+% surf(den)
+% shading interp
