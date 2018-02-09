63c43add4232ed2e364f59aaa4934de1aea70664
Christopher Ng <christopherng1990@gmail.com>
2018-02-09 14:25:36 -0500

    Assignment 2 code

---
+++ Assignment 2/code/A2Part1b.m
@@ -0,0 +1,47 @@
+close all
+clear
+
+nx = 50;
+ny = nx*3/2;
+G = sparse(nx*ny);
+V = zeros(nx*ny, 1);
+
+for i = 1:nx
+    for j = 1:ny
+        n = j + (i-1)*ny;
+        
+        if i == 1
+            G(n, :) = 0;
+            G(n, n) = 1;
+            V(n) = 1;
+        elseif i == nx
+            G(n, :) = 0;
+            G(n, n) = 1;
+            V(n) = 1;
+        elseif j == 1
+            G(n, :) = 0;
+            G(n, n) = 1;
+        elseif j == ny
+            G(n, :) = 0;
+            G(n, n) = 1;
+        else
+            G(n, n) = -4;
+            G(n, n+1) = 1;
+            G(n, n-1) = 1;
+            G(n, n+ny) = 1;
+            G(n, n-ny) = 1;
+        end
+    end
+end
+
+E = G\V;
+
+figure(2)
+X = zeros(nx, ny, 1);
+for i = 1:nx
+    for j = 1:ny
+        n = j + (i-1)*ny;
+        X(i,j) = E(n);
+    end
+end
+surf(X)
