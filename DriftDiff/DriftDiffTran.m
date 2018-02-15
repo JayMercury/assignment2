183652cdb79ce7909d20bc0e07e13844fd4f1962
Christopher Ng <christopherng1990@gmail.com>
2018-02-15 17:51:09 -0500

    PA6

--- DriftDiff/DriftDiffTran.m
+++ DriftDiff/DriftDiffTran.m
@@ -38,8 +38,8 @@ PlotFile = 'image.gif';
 PlotCount = 0;
 doPlotImage = 0; % set to 1 to draw the image
 
-Simulation = 'PNJctEqBias';
-% Simulation = 'GaussianTwoCarRCLinGrad';
+% Simulation = 'PNJctEqBias';
+Simulation = 'PA6_2';
 
 if strcmp(Simulation,'GaussianTwoCar')
     eval('SetGaussian2CarParas');
@@ -59,6 +59,10 @@ elseif strcmp(Simulation,'PNJctEq')
     eval('SetPNJctParasEqBC');
 elseif strcmp(Simulation,'PNJctEqBias')
     eval('SetPNJctParasEqBCBias');
+elseif strcmp(Simulation,'PA6_1')
+    eval('PA6_1');
+elseif strcmp(Simulation,'PA6_2')
+    eval('PA6_2');
 end
 
 FormGv(nx,LVbc,RVbc); % Poisson equation set Gv and Bv
